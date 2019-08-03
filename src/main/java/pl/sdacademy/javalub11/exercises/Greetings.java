09fb6fa29e16e383d21c74bfd4f9c7bf2f0b5d31
Artur Wróblewski <artur.wroblewski_89@o2.pl>
2019-08-03 14:33:55 +0200

    first commit

--- src/main/java/pl/sdacademy/javalub11/exercises/Greetings.java
+++ src/main/java/pl/sdacademy/javalub11/exercises/Greetings.java
@@ -3,6 +3,9 @@ package pl.sdacademy.javalub11.exercises;
 public class Greetings {
 
     public String greet(String name) {
+        if (name == null){
+            return "Witaj, mój przyjacielu!";
+        } else
         return "Witaj, " + name;
     }
 
--- src/test/java/pl/sdacademy/javalub11/exercises/GreetingsTest.java
+++ src/test/java/pl/sdacademy/javalub11/exercises/GreetingsTest.java
@@ -20,5 +20,19 @@ public class GreetingsTest {
 
     }
 
+    @Test
+    public void testShouldReturnFriendlyGreetingIfNull(){
+        //given
+        Greetings greetings = new Greetings();
+        String name = null;
+        //when
+        String result = greetings.greet(name);
+
+        //then
+        assertEquals("Witaj, mój przyjacielu!", result);
+
+    }
+
+
 
 }
