a68dcc96ba7e86a32f15857474bfea513bf36c9e
ukorpysz <ukorpysz@ukorpysz.com>
2019-08-03 15:36:37 +0200

    Zadanie 1. Wymaganie 4

--- src/main/java/pl/sdacademy/javalub11/exercises/Greetings.java
+++ src/main/java/pl/sdacademy/javalub11/exercises/Greetings.java
@@ -3,12 +3,12 @@ package pl.sdacademy.javalub11.exercises;
 public class Greetings {
 
     public String greet(String name) {
-        String name2=name.replace(",", " i ");
+        String name2 = name.replace(",", " i ");
 
         if (name == null) {
             return "Witaj, mój przyjcielu";
         } else {
-            if ( name2!= null) {
+            if (name2 != null) {
                 name = name2;
                 return name + ", witajcie!";
             }
