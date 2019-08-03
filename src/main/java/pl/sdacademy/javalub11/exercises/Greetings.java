b6ee17834365fe4815dfcfaefc77aae44db826aa
lkapiec <lkapiec@gmail.com>
2019-08-03 22:15:39 +0200

    Requirement 5 finished, refactoring need to be done.

--- src/main/java/pl/sdacademy/javalub11/exercises/Greetings.java
+++ src/main/java/pl/sdacademy/javalub11/exercises/Greetings.java
@@ -2,51 +2,99 @@ package pl.sdacademy.javalub11.exercises;
 
 public class Greetings {
 
-  public String greet(String name) {
-    boolean isMultipleNames = false;
+  /* TODO should throw exception */
+  private boolean isUpper(String name) {
+    if (name == null) return false;
+    return name.equals(name.toUpperCase());
+  }
 
+  public int countUpper(String name)
+  {
+    int result = 0;
+
+    String[] names = name.split("\\,");
+
+    for (String s : names) {
+      if (isUpper(s)) result++;
+    }
+
+    return result;
+  }
+
+  private String processNames(String name) {
+    String resultLeft = "";
+    String resultRight = "";
+
+    String[] names = name.split("\\,");
+
+    int namesAmount = names.length;
+    int namesRightAmount = countUpper(name);
+    int namesLeftAmount = namesAmount - namesRightAmount;
+    int indexLeft = 0;
+    int indexRight = 0;
+
+    for (String s : names) {
+
+      if (isUpper(s)) {
+        if (indexRight > 0) {
+          if (indexRight == (namesRightAmount - 1)) {
+            resultRight += " i ";
+          } else {
+            resultRight += ", ";
+          }
+        }
+        resultRight += s.trim();
+        indexRight++;
+
+      } else {
+
+        if (indexLeft > 0) {
+          if (indexLeft == (namesLeftAmount - 1)) {
+            resultLeft += " i ";
+          } else {
+            resultLeft += ", ";
+          }
+        }
+        resultLeft += s.trim();
+
+        indexLeft++;
+      }
+    }
+
+    //System.out.println("In test: left='" + resultLeft + "' right='" + resultRight + "'");
+
+    if (resultLeft.length() > 0)
+    {
+      if (indexLeft > 1) {
+        resultLeft += ", witajcie!";
+      }else{
+        resultLeft = "Witaj, " + resultLeft;
+      }
+    }
+
+    if (resultRight.length() > 0)
+    {
+      if (indexRight > 1)
+      {
+        resultRight = "WITAJCIE " + resultRight + "!";
+      } else {
+        resultRight = "WITAJ " + resultRight + "!";
+      }
+    }
+
+    String separator = "";
+    if((resultLeft.length() > 0) && (resultRight.length() > 0))
+      separator = " ";
+
+    return resultLeft + separator + resultRight;
+  }
+
+  public String greet(String name) {
     String result = "Witaj, mój przyjacielu";
 
     if (name == null) return result;
 
-    if (name.contains(",")) isMultipleNames = true;
-
-    if (isMultipleNames) {
-      String[] names = name.split("\\,");
-
-      int namesAmount = names.length;
-
-      // Adam i Iza, witajcie!
-      result = "";
-      int index = 0;
-
-      for (String s : names) {
-
-        if (index > 0) {
-            //dla ostatniego dodajem i w przeciwnym wypadku tylko przecinek
-          if (index == (namesAmount - 1)) {
-            result += " i ";
-          } else {
-            result += ", ";
-          }
-        }
-        result += s;
-
-        index++;
-      }
-
-      result += ", witajcie!";
-
-
-
-    } else {
-      String upperCase = name.toUpperCase();
-      if (name.equals(upperCase)) {
-        result = "WITAJ, " + upperCase + "!";
-      } else {
-        result = "Witaj, " + name;
-      }
-    }
+    result = processNames(name);
     return result;
   }
 }
--- src/test/java/pl/sdacademy/javalub11/exercises/GreetingsTest.java
+++ src/test/java/pl/sdacademy/javalub11/exercises/GreetingsTest.java
@@ -7,6 +7,66 @@ import static org.junit.Assert.*;
 public class GreetingsTest {
 
   @Test
+  public void testCountUpperWithOutProperName()
+  {
+    Greetings greetings = new Greetings();
+
+    //given
+    String name = "Adam";
+
+    //when
+    int count = greetings.countUpper(name);
+
+    //then
+    assertEquals(0, count);
+  }
+
+  @Test
+  public void testCountUpperWithProperName()
+  {
+    Greetings greetings = new Greetings();
+
+    //given
+    String name = "ADAM";
+
+    //when
+    int count = greetings.countUpper(name);
+
+    //then
+    assertEquals(1, count);
+  }
+
+  @Test
+  public void testCountUpperWithAndWithOutProperName()
+  {
+    Greetings greetings = new Greetings();
+
+    //given
+    String name = "ADAM, Kasia";
+
+    //when
+    int count = greetings.countUpper(name);
+
+    //then
+    assertEquals(1, count);
+  }
+  @Test
+  public void testCountUpperWithProperNameRepeatedTwice()
+  {
+    Greetings greetings = new Greetings();
+
+    //given
+    String name = "ADAM, KATARZYNA";
+
+    //when
+    int count = greetings.countUpper(name);
+
+    //then
+    assertEquals(2, count);
+  }
+
+  /* requirement 1 */
+  @Test
   public void testShouldReturnProperGreeting() {
     // given
     Greetings greetings = new Greetings();
@@ -19,6 +79,7 @@ public class GreetingsTest {
     assertEquals("Witaj, " + name, result);
   }
 
+  /* requirement 1 */
   @Test
   public void testShouldReturnProperGreetingforSecondCase() {
     // given
@@ -32,6 +93,7 @@ public class GreetingsTest {
     assertEquals("Witaj, " + name, result);
   }
 
+  /* requirement 2 */
   @Test
   public void testShouldCheckProtectionAgainstNullValue() {
     // given
@@ -45,6 +107,7 @@ public class GreetingsTest {
     assertEquals("Witaj, mój przyjacielu", result);
   }
 
+  /* requirement 3 */
   @Test
   public void testShouldUpperCaseGreetingPrefixOnceParamIsUpperCaseTooFirstCase() {
     // given
@@ -55,9 +118,10 @@ public class GreetingsTest {
     String result = greetings.greet(name);
 
     // then
-    assertEquals("WITAJ, ADAM!", result);
+    assertEquals("WITAJ ADAM!", result);
   }
 
+  /* requirement 3 */
   @Test
   public void testShouldUpperCaseGreetingPrefixOnceParamIsUpperCaseTooSecondCase() {
     // given
@@ -68,9 +132,10 @@ public class GreetingsTest {
     String result = greetings.greet(name);
 
     // then
-    assertEquals("WITAJ, TOMEK!", result);
+    assertEquals("WITAJ TOMEK!", result);
   }
 
+  /* requirement 4 */
   @Test
   public void testShouldCheckIfMehodWorksCorrectlyTwoParameters() {
     // given
@@ -84,6 +149,7 @@ public class GreetingsTest {
     assertEquals("Adam i Iza, witajcie!", result);
   }
 
+  /* requirement 5 */
   @Test
   public void testShouldCheckIfMehodWorksCorrectlyThreeParameters() {
     // given
@@ -95,5 +161,34 @@ public class GreetingsTest {
 
     // then
     assertEquals("Adam, Iza i Robert, witajcie!", result);
+  }
+
+  /* requirement 5  */
+  @Test
+  public void testShouldCheckIfMehodWorksCorrectlyForMoreThanThreeParameters() {
+    // given
+    Greetings greetings = new Greetings();
+    String name = "AA,BB,CC,DD";
+
+    // when
+    String result = greetings.greet(name);
+
+    // then
+    assertEquals("WITAJCIE AA, BB, CC i DD!", result);
+  }
+
+
+  /* requirement 6 */
+  @Test
+  public void testShouldCheckIfMehodWorksForDifferentUpperCaseNames() {
+    // given
+    Greetings greetings = new Greetings();
+    String name = "Adam, Iza, ROBERT";
+
+    // when
+    String result = greetings.greet(name);
+
+    // then
+    assertEquals("Adam i Iza, witajcie! WITAJ ROBERT!", result);
   }
 }
